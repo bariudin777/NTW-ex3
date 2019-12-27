2abf024dce32718cd0ceac4dc25eccacf02276a9
ElielLopez <elielsw1@gmail.com>
2019-12-27 14:49:33 +0200

    commit

--- server.py
+++ server.py
@@ -1,102 +1,141 @@
-import os  # TODO problem???
-import sys
-from socket import *
+import socket
+import select
+import os
 
 
-# do i need here singleton??
+def main(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    s.listen(5)
+    print('Server listening on port %d' % port)
+    while True:
+        conn, addr = s.accept()
+        keep_alive = True
+
+        while keep_alive:
+            keep_alive = False
+
+            ready = select.select([conn], [], [], 1)
+            if not ready[0]:
+                # timeout
+                print('Timeout for {0}'.format(addr))
+                continue
+            try:
+                msg = conn.recv(1024).decode('utf-8')
+                print('_' * 10)
+                print(msg)
+                print('=' * 10)
+                req = parse_request(msg)
+                keep_alive = handle_request(req, conn)
+            except Exception as e:
+                print('ERROR:', e)
+                conn.send(
+                    'HTTP/1.1 500 Internal Server Error\r\nConnection:close\r\nContent-Length: 3\r\n\r\nISE'.encode(
+                        'utf-8'))
+                keep_alive = False
+        conn.close()
 
 
-class Server:
+def handle_request(req, conn):
+    if req['method'] != 'GET':
+        # not supported
+        # theoretically - send back "not supported" response.
+        print('Unknown method', req['method'])
+        return False
 
-    def __init__(self):
-        self.file_name = "/"
-        self.port = 8080  # defulte value
-        self.server = socket(AF_INET, SOCK_STREAM)
-        self.path = ""
+    route = req['route']
+    connection = req['connection']
+    if route == '/redirect':
+        res = 'HTTP/1.1 301 Moved Permenantly\r\nConnection: close\r\nLocation: /result.html\r\n\r\n'
+        conn.send(res.encode('utf-8'))
+        return False
 
-    # connect to the client- preform bind operation to ip and port
-    def connect(self):
-        server_ip = '127.0.0.1'
-        self.server.bind((server_ip, int(self.port)))
-        self.server.listen(5)
+    if route == '/':
+        route = '/index.html'
+    route = './files' + route
+    if not file_exists(route):
+        # file not found - 404.
+        res = 'HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n'
+        conn.send(res.encode('utf-8'))
+        return False
 
-    # sends the data directly
-    def sendR(self):
-        client_socket, client_address = self.server.accept()
-        self.send(self.file_name.encode())
+    if route.endswith('.ico') or route.endswith('.jpg'):
+        # send binary
+        payload = readfile(route, 'rb')
+    else:
+        payload = readfile(route, 'r')
+        payload = payload.encode('utf-8')
 
-    # sends the data binary
-    def sendB(self):
-        while True:
-            client_socket, client_address = self.server.accept()
-            f = open(self.file_name, 'rb')
-            f.seek(0)
-            payload = f.read(1024)
-            while payload:
-                client_socket.sendall(payload)
-                payload = f.read(1024)
-            f.close()
-
-    #  TODO- what server returns
-    def prosseceClinetOutput(self):
-        pass
-
-    # connect and down load the file
-    def download(self):
-        # start connection
-        self.connect()
-        # init all files in base folder to list
-        data = os.listdir(str(self.path))
-        # if there such file- ask if it jpg/ico- send binary, otherwise send the file as is
-        if self.file_name in data:
-            # check if the postfix is jpg/ico
-            if "jpg" in self.file_name or "ico" in self.file_name:
-                # send binary
-                self.sendB()
-                # print the data
-                self.prosseceClinetOutput()
-            else:
-                # send diractly
-                self.sendR()
-                # print the data
-                self.prosseceClinetOutput()
-
-    # create a legal path for download
-    def create_path(self, input_list, size):
-        path = ""
-        if input_list[0] is "/":
-            path = "/"
-            return "./files" + path
-        path_list = input_list[1:size - 1]
-        for i in path_list:  # init path
-            path += i
-            path += "/"
-        path = path[:len(self.path) - 1]  # get rid of the last "/"
-        return "./files/" + path
-
-    # prossece the input - init the file name, port and path
-    def prosseceServerInput(self, filename):
-
-        input_list = filename.split('/')  # ["8080","a","oh_no.jpg" ]
-        size = len(input_list)
-        # init port from input
-        default_port = 8080
-        if isinstance(int(input_list[0]), int):  # init the port
-            self.port = input_list[0]
-        # init path from input
-        self.path = self.create_path(input_list, size)
-        # init file name from input
-        if self.path is "/":
-            self.file_name = "‫‪index.html‬‬"
-            return self.file_name
-        else:
-            self.file_name = input_list[size - 1]
-            return self.file_name  # return file name
+    res_header = 'HTTP/1.1 200 OK\r\nConnection: {0}\r\nContent-Length: {1}\r\n\r\n'.format(connection, len(payload))
+    conn.send(res_header.encode('utf-8'))
+    conn.send(payload)
+    return req['connection'] == 'keep-alive'
 
 
-if __name__ == "__main__":
-    # input = sys.argv[0]  # for example: localhost:8080/a/oh_no.jpg----> i will recive 8080/a/oh_no.jpg
-    input = "8080/a/oh_no.jpg"
-    server = Server()
-    file_name = server.prosseceServerInput(input)
-    server.download()
+def file_exists(filename):
+    return os.path.exists(filename)
+
+
+def readfile(filename, mode):
+    f = open(filename, mode)
+    data = f.read()
+    f.close()
+    return data
+
+
+def parse_header(header):
+    headers = header.split('\r\n')
+    method, route, _ = headers[0].split(' ')
+
+    connection = None
+    for header in headers:
+        if header.startswith('Connection:'):
+            _, connection = header.split(':', 1)
+            connection = connection.strip()
+            break
+    # no connection field - throw exeption
+    if connection is None:
+        raise Exception('No connection header')
+
+    return {
+        'method': method,
+        'route': route,
+        'connection': connection
+    }
+
+
+def parse_request(req):
+    header = ''
+    split = req.split('\r\n\r\n', 1)
+    header = parse_header(split[0])
+    return header
+
+
+status_description = {
+    200: 'OK',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not Found',
+    500: 'Internal Server Error',
+    503: 'Service Unavailable'
+}
+
+
+def HTTPHeader(status_code, content_type, body):
+    header = []
+    header.append('HTTP/1.1 {0} {1}'.format(status_code, status_description[status_code]))
+    header.append('Content-Length: {0}'.format(len(body)))
+    header.append('Connection: close')
+    header.append('Content-Type: ' + content_type)
+    return '\r\n'.join(header)
+
+
+def response_404():
+    return 'HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n'
+
+
+if __name__ == '__main__':
+    import sys
+
+    main(int(sys.argv[1]))
